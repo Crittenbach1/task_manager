4b7ba4436def45ba395ffdbbb4075d727ebefd5c
Cynthia Rittenbach <crittenbach@gmail.com>
2017-11-27 07:19:47 +0000

    edited nested task in controller and added <br> in form

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,9 +8,10 @@ class ListsController < ActionController::Base
   end
 
   def create
-    @list = List.new(list_params)
+    @list = List.new(title: params[:list][:title])
+    @task = @list.tasks.new(title: params[:task][:title], content: params[:task][:content])
    binding.pry
-    if @list.save
+    if @list.save && @task.save
       redirect_to list_path(@list)
     else
       render :new
