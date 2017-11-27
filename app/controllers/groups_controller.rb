a049c11e83fa936b10e7cc3d3bfa9228d03d25d9
Cynthia Rittenbach <crittenbach@gmail.com>
2017-11-27 01:02:46 +0000

    added group form validations, index links to groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,10 +8,9 @@ class GroupsController < ActionController::Base
   end
 
   def create
-    @group = Group.new
-
-    if @group
-      @group.save
+    @group = Group.new(group_params)
+  #  binding.pry
+    if @group.save
       redirect_to group_path(@group)
     else
       render :new
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ActiveRecord::Base
+    validates :title, presence: true
     has_many :users
     has_many :lists
     has_many :tasks, through: :lists
