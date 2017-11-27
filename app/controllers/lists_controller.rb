895d402c8d629d0f833d14d3f683a5694a172a51
Cynthia Rittenbach <crittenbach@gmail.com>
2017-11-27 01:15:45 +0000

    created list controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ActionController::Base
+  def index
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+  #  binding.pry
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  private
+
+      def list_params
+        params.require(:list).permit(:title)
+      end
+
+end
