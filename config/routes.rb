545c1fa0e426a566003771412a8b888f3b85532a
Cynthia Rittenbach <crittenbach@gmail.com>
2017-11-27 00:39:20 +0000

    added new group form, index, links to them

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-
+  resources :groups, only: [:index, :new, :create]
+  
   devise_for :users
 
   devise_scope :user do
-     get '/users/sign_out' => 'devise/sessions#destroy'     
+     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
   root 'user#index'
